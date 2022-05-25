a233037a0bfd8a305a729024ed735ac51b1f3d05
Mohammad Rahman <razibur@outlook.com>
2022-05-25 10:20:08 -0400

    add data access to precipitation service

--- cloudweather.precipitation/Program.cs
+++ cloudweather.precipitation/Program.cs
@@ -1,12 +1,32 @@
+using cloudweather.precipitation.DataAccess;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<PrecipDbContext>(
+    opts =>
+    {
+        opts.EnableSensitiveDataLogging();
+        opts.EnableDetailedErrors();
+        opts.UseNpgsql(builder.Configuration.GetConnectionString("AppDb"));
+    },ServiceLifetime.Transient
+ );
+
 var app = builder.Build();
 
-app.MapGet("/observation/{zip}", (string zip, [FromQuery] int? days) =>
+app.MapGet("/observation/{zip}", async (string zip, [FromQuery] int? days, PrecipDbContext db) =>
  {
-     return Results.Ok(zip);
+     if (days == null || days <1 || days >30)
+     {
+         return Results.BadRequest("Please provide a 'days' query parameter between 1 and 30");
+     }
+     var startDate = DateTime.UtcNow - TimeSpan.FromDays(days.Value);
+     var results = await db.Precipitation
+        .Where(precip => precip.Zipcode == zip && precip.CreatedOn > startDate)
+        .ToListAsync();
+
+     return Results.Ok(results);
  });
 
 app.Run();
