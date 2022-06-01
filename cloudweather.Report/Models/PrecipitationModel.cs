d5a794bcaf90d1c3bbce183aa6ac5296e92e9d33
Mohammad Rahman <razibur@outlook.com>
2022-05-31 20:18:10 -0400

    Add weather report microservice logic

---
+++ cloudweather.Report/Models/PrecipitationModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Cloudweather.Report.Models
+{
+    public class PrecipitationModel
+    {
+        public decimal AmountInches { get; set; }
+        public string WeatherType { get; set; }
+    }
+}
