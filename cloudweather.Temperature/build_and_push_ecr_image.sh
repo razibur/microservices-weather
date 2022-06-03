76b5d9dd7b188cfb99ee79006566d0d90751d017
Mohammad Rahman <razibur@outlook.com>
2022-06-03 12:50:26 -0400

    Add Dockerfiles and scripts

---
+++ cloudweather.Temperature/build_and_push_ecr_image.sh
@@ -0,0 +1,6 @@
+﻿#
+set -e
+aws ecr get-login-password --region us-east-1 --profile weather-ecr-agent | docker login --username AWS --password stdin 622810360479.dkr.ecr.us-east-1.amazonaws.com
+docker build -f ./Dockerfile -t cloud-weather-temperature:latest .
+docker tag cloud-weather-temperature:latest 622810360479.dkr.ecr.us-east-1.amazonaws.com/cloud-weather-temperature:latest
+docker push 622810360479.dkr.ecr.us-east-1.amazonaws.com/cloud-weather-temperature:latest 
---
+++ cloudweather.precipitation/build_and_push_ecr_image.sh
@@ -0,0 +1,6 @@
+﻿#
+set -e
+aws ecr get-login-password --region us-east-1 --profile weather-ecr-agent | docker login --username AWS --password stdin 622810360479.dkr.ecr.us-east-1.amazonaws.com
+docker build -f ./Dockerfile -t cloud-weather-precipitation:latest .
+docker tag cloud-weather-precipitation:latest 622810360479.dkr.ecr.us-east-1.amazonaws.com/cloud-weather-precipitation:latest
+docker push 622810360479.dkr.ecr.us-east-1.amazonaws.com/cloud-weather-precipitation:latest 
